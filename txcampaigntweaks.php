c6f2a484611f1b2ab4b13286cd5ba718c73358c8
Allen Shaw <allen@JoineryHQ.com>
2024-12-26 17:30:13 -0600

    civix upgrade for civix format 23.02.1

--- txcampaigntweaks.php
+++ txcampaigntweaks.php
@@ -79,59 +79,12 @@ function txcampaigntweaks_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function txcampaigntweaks_civicrm_postInstall() {
-  _txcampaigntweaks_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function txcampaigntweaks_civicrm_uninstall() {
-  _txcampaigntweaks_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function txcampaigntweaks_civicrm_enable() {
   _txcampaigntweaks_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function txcampaigntweaks_civicrm_disable() {
-  _txcampaigntweaks_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function txcampaigntweaks_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _txcampaigntweaks_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function txcampaigntweaks_civicrm_entityTypes(&$entityTypes) {
-  _txcampaigntweaks_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
